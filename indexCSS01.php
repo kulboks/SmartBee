7dd0469f19eeee427310b804960be8571e4565f6
kulboks <kulboks@gmail.com>
2019-04-03 15:00:10 +0300

    dsfrdrfd

---
+++ indexCSS01.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CSS01</title>
+    <meta charset="utf-8">
+    
+    <link href="style.css" rel="stylesheet" type="text/css">
+    
+    <style>
+        h2 {
+            background-color: rgb(0, 255, 0);
+        }
+    </style>
+</head>
+<body>
+    <article>
+        <h2 id="p1">Lorem ipsum</h2>
+        <p>
+            Heim machs gut heim Chinette ech hopla geiss met hopla geiss, vielmols picon 
+            bière pfourtz!, heim guata mes schlopp, égàl vater vater knepfle rossbolla, 
+            bisame bibbeleskaas fleishwurcht, sürrkrut bisame elsassisch munster, saucisse 
+            bür gross. Melfor wenn Chinette doch Yeeuuu wenn de Huguette le mannele voll  a 
+            salami bisous le Chinette, rossbolla am bredele glühwein. Gal mir 
+            Waltenuntenweyersniederwiller égàl gal chenerator tchao bissame dûû gànz bisame 
+            Huguette net chambon klein fraü le hopla. Knack Sepala le Flàmmeküeche, a bisame 
+            bier Ponchour elsassisch, Coopé schissabibala met bisame Yeeuuu, wurscht alles 
+            Boumbernikel oder gross picon bière jetz yeuh knèkes, Carola pfourtz! 
+            Boumbernikel, net schneck un schluck Niederhausbergen, gànz knepfle mes schlopp 
+            saucisse. Bier sürrkrut S'ech geut de choucroute bredele hopla mir knepfle. De 
+            picon bière yeuh bett sürrkrut eme vielmols mitt knäckes. Gal! knepfle noch 
+            bier, guata saucisse sech Ftomi! bibbala.
+        </p>
+    </article>
+
+    <article>
+        <h2 class="red">Lorem ipsum</h2>
+        <p>
+            Heim machs gut heim Chinette ech hopla geiss met hopla geiss, vielmols picon 
+            bière pfourtz!, heim guata mes schlopp, égàl vater vater knepfle rossbolla, 
+            bisame bibbeleskaas fleishwurcht, sürrkrut bisame elsassisch munster, saucisse 
+            bür gross. Melfor wenn Chinette doch Yeeuuu wenn de Huguette le mannele voll  a 
+            salami bisous le Chinette, rossbolla am bredele glühwein. Gal mir 
+            Waltenuntenweyersniederwiller égàl gal chenerator tchao bissame dûû gànz bisame 
+            Huguette net chambon klein fraü le hopla. Knack Sepala le Flàmmeküeche, a bisame 
+            bier Ponchour elsassisch, Coopé schissabibala met bisame Yeeuuu, wurscht alles 
+            Boumbernikel oder gross picon bière jetz yeuh knèkes, Carola pfourtz! 
+            Boumbernikel, net schneck un schluck Niederhausbergen, gànz knepfle mes schlopp 
+            saucisse. Bier sürrkrut S'ech geut de choucroute bredele hopla mir knepfle. De 
+            picon bière yeuh bett sürrkrut eme vielmols mitt knäckes. Gal! knepfle noch 
+            bier, guata saucisse sech Ftomi! bibbala.
+        </p>
+    </article>
+
+</body>
+</html>
